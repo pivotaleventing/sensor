a0cd9a0f6424fb0ff62771838f57deaf9f6e444d
Stefan Sonnenberg-Carstens <stefan.sonnenberg.carstens@gmail.com>
2022-11-15 14:23:32 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+from fastapi import FastAPI
+from uvicorn import run
+
+
+app = FastAPI()
+
+@app.get('/', response_model=dict)
+async def index() -> dict:
+    return {'msg': 'Hello, Google Cloud Run!')
+
+
+if __name__ == '__main__':
+    run(app=app, host='', port=8080)
