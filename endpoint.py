438ee5c2c490fea8b034928bb3dcca5285052201
Stefan Sonnenberg-Carstens <stefan.sonnenberg.carstens@gmail.com>
2022-11-15 14:33:03 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -9,5 +9,10 @@ async def index() -> dict:
     return {'msg': 'Hello, Google Cloud Run!'}
 
 
+@app.post('/sensor/', response_model=dict)
+async def post_sensor_data(payload: dict) -> dict:
+    return payload
+
+
 if __name__ == '__main__':
     run(app=app, host='', port=8080)
